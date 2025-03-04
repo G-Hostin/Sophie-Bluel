b0793221e8bd059e9d81c5718d61c981ad5bdc06
Gregory Hostin <greghostin@icloud.com>
2025-03-04 18:58:00 +0100

    Complete modal integration: display works, handle uploads, and style elements

--- FrontEnd/admin.js
+++ FrontEnd/admin.js
@@ -1,36 +1,38 @@
-const token = window.localStorage.getItem("token");
-const loginElement = document.getElementById("login-header");
+const token = window.localStorage.getItem("token"); // Recupere le token qu'on à stocké dans localStorage après le login
+const loginElement = document.getElementById("login-header"); // Recupere le li qui correspond à "login/logout" dans le header
 
 if (token) {
-  const body = document.body;
-  const editMode = document.createElement("div");
-  editMode.classList.add("edit-mode");
-  editMode.innerHTML = `<i class="fa-regular fa-pen-to-square"></i><p>Mode édition<p>`;
-  body.insertBefore(editMode, body.firstChild);
+  // Si le token est ok
+  const body = document.body; // Stocke le body dans une variable body
+  const editMode = document.createElement("div"); // stocke une div qui vient d'être créée dans une variable editMode
+  editMode.classList.add("edit-mode"); // Ajoute la classe edit-mode pour cette div (pour le css)
+  editMode.innerHTML = `<i class="fa-regular fa-pen-to-square"></i><p>Mode édition<p>`; // Ajoute l'icône et le texte dans cette div
+  body.insertBefore(editMode, body.firstChild); // Insere la div editMode avant le premier enfant de body (elle devient donc le premier enfant)
 
-  const portfolio = document.getElementById("portfolio");
-  const portfolioTitle = document.querySelector("#portfolio h2");
+  const portfolio = document.getElementById("portfolio"); // Recupere la section portfolio
+  const portfolioTitle = document.querySelector("#portfolio h2"); // Recupere le titre de la section portfolio
 
-  const editProject = document.createElement("div");
-  editProject.classList.add("edit-project");
+  const editProject = document.createElement("div"); // Créé et stocke une div dans editProject
+  editProject.classList.add("edit-project"); // Ajoute la classe edit-project à cette div
 
-  portfolio.insertBefore(editProject, portfolio.firstChild);
-  editProject.appendChild(portfolioTitle);
+  portfolio.insertBefore(editProject, portfolio.firstChild); // Insere la div editProject avant le premier enfant de portfolio (elle devient donc le premier enfant)
+  editProject.appendChild(portfolioTitle); // Place le titre dans cette div
   editProject.insertAdjacentHTML(
     "beforeend",
     `<a href="#" id="edit-btn" target="_blank"><i class="fa-regular fa-pen-to-square"></i><p>modifier</p></a>`
-  );
+  ); // Insere le HTML (lien avec une icone et du texte) après le titre
 
-  const filtersContainer = document.querySelector(".filters");
-  filtersContainer.style.display = "none";
+  const filtersContainer = document.querySelector(".filters"); // Recupere la div qui contient les boutons de filtres
+  filtersContainer.style.display = "none"; // N'affiche plus les boutons de filtres
 
-  loginElement.innerHTML = `<a href="#" id="logout-link">logout</a>`;
-  const logoutLink = document.getElementById("logout-link");
+  loginElement.innerHTML = `<a href="#" id="logout-link">logout</a>`; // Remplace le HTML du li loginElement par un lien avec ecrit logout (qui a un id logout-link)
+  const logoutLink = document.getElementById("logout-link"); //Recupère le logout-link
   logoutLink.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.localStorage.clear();
-    window.location.reload();
+    // Ajoute un event listener
+    event.preventDefault(); // Empeche le comportement par défault
+    window.localStorage.clear(); // Nettoie/supprime le localStorage de la page
+    window.location.reload(); // Recharge la page (il n'y aura donc plus de localStorage et la condition if ligne 4 n'est plus vraie)
   });
 } else {
-  loginElement.innerHTML = `<a href="login.html">login</a>`;
+  loginElement.innerHTML = `<a href="login.html">login</a>`; // Le texte login devient un lien qui renvoie vers la page login.html
 }
--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,29 +1,79 @@
-const editBtn = document.getElementById("edit-btn");
-const modal = document.getElementById("modalBox");
-const modalBody = document.querySelector(".modal-body");
-const closeBtn = document.querySelector(".modal-closeBtn");
-const modalTitle = document.querySelector(".modal-title");
-const modalWorks = document.querySelector(".modal-works");
-const addBtn = document.getElementById("addPhotoBtn");
+const editBtn = document.getElementById("edit-btn"); // Recupere le bouton modifier pour ouvrir la modale
+const modal = document.getElementById("modalBox"); // Recupere la modale
+const modalBody = document.querySelector(".modal-body"); // Recupere le body de la modale
+const closeBtn = document.querySelector(".modal-closeBtn"); // Recupere la croix pour fermer la modale
+const closeBtnForm = document.querySelector(".modal-closeBtnForm"); // Recupere la croix pour fermer la deuxième modale (formulaire)
+const modalTitle = document.querySelector(".modal-title"); // Recupere le titre de la modale
+const modalWorks = document.querySelector(".modal-works"); // Recupere la div pour afficher les travaux de la modale
+const addBtn = document.getElementById("addPhotoBtn"); // Recupere le bouton pour ajouter une photo
+const backBtn = document.querySelector(".modal-previous"); // Recupere l'icone de fleche pour revenir à la premiere modale
+const modalGallery = document.getElementById("modalGallery"); // Recupere la div qui contient la premiere modale
+const modalForm = document.getElementById("modalForm"); // Recupere la div qui contient la deuxieme modale
 
 editBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  modal.style.display = "flex";
-  document.body.style.overflow = "hidden";
+  // Ajoute un event listener pour le bouton modifier pour ouvrir la modale
+  event.preventDefault(); // Empeche le comportement par default
+  modal.style.display = "flex"; // Affiche la modale (qui a un display none dans son HTML)
+  document.body.style.overflow = "hidden"; // Empeche le scroll sur le body quand la modale est ouverte
+  displayWorksModal(); // Appelle la fonction qui affiche les travaux
 });
 
 closeBtn.addEventListener("click", () => {
-  modal.style.display = "none";
-  document.body.style.overflow = "auto";
+  // Ajoute un event listener sur la croix pour fermer la modale
+  modal.style.display = "none"; // N'affiche plus la modale
+  document.body.style.overflow = "auto"; // Réactive le scroll sur le body
+});
+
+closeBtnForm.addEventListener("click", () => {
+  // Ajoute un event listener sur la croix pour fermer la deuxieme modale (formulaire)
+  modalForm.style.display = "none"; // N'affiche plus la deuxieme modale (formulaire)
+  modalGallery.style.display = "flex"; // Réaffiche la premiere modale
+  modal.style.display = "none"; // N'affiche plus la modale
+  document.body.style.overflow = "auto"; // Réactive le scroll sur le body
 });
 
 modal.addEventListener("click", (event) => {
+  // Ajoute un event listener sur toute la modale
   if (event.target === modal) {
-    modal.style.display = "none";
-    document.body.style.overflow = "auto";
+    // Si le click est sur modal (zone sombre opacité 80% dans le fond, la "vraie" modale est modalBody)
+    modalForm.style.display = "none"; // N'affiche plus la deuxieme modale (formulaire)
+    modalGallery.style.display = "flex"; // Réaffiche la premiere modale
+    modal.style.display = "none"; // N'affiche plus la modale
+    document.body.style.overflow = "auto"; // Réactive le scroll sur body
   }
 });
 
 modalBody.addEventListener("click", (event) => {
-  event.stopPropagation();
+  // Ajoute un event listener sur modalBody (la "vraie" modale)
+  event.stopPropagation(); // Empeche la propagation de l'evenement click (la click ne va pas se propager à son parent (modal) qui aurait provoqué la fermeture de la modale)
+});
+
+function displayWorksModal() {
+  // Fonction pour afficher les travaux dans la modale
+  modalWorks.innerHTML = ""; // Vide la div qui doit contenir les travaux dans la modale
+
+  globalWorks.forEach((work) => {
+    // Parcours chaque travail de globalWorks
+    const workElement = document.createElement("div"); // Créé une div workElement pour chaque travail
+    workElement.classList.add("work-element"); // Ajoute une classe work-element à la div pour le css
+    workElement.innerHTML = `
+      <img src="${work.imageUrl}" alt="${work.title}">
+      <div class="delete-btn">
+        <i class="fa-solid fa-trash-can"></i>
+      </div>
+    `; // Ajoute l'url de l'image et la balise alt dans la div workElement créée pour chaque travail + une div qui contient l'icone de poubelle
+    modalWorks.appendChild(workElement); // La div créée devient l'enfant du container qui doit contenir les travaux dans la modale
+  });
+}
+
+addPhotoBtn.addEventListener("click", () => {
+  // On ajoute un event listener au bouton Ajouter une photo de la modale
+  modalGallery.style.display = "none"; // Au click, fait disparaitre la premiere modale (vue gallerie)
+  modalForm.style.display = "flex"; // Toujours sur ce même click, fait apparaitre la deuxieme modale (formulaire)
+});
+
+backBtn.addEventListener("click", () => {
+  // On ajoute un event listener au bouton Fleche de retour de la deuxieme modale
+  modalForm.style.display = "none"; // Au click, fait disparaitre la deuxieme modale (formulaire)
+  modalGallery.style.display = "flex"; // Toujours sur ce même click, fait apparaitre la premiere modale (vue gallerie)
 });
